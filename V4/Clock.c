f499da2e28ee54bbdc9e7fc198b916ffe1bccdcf
UO247346 <uo247346@uniovi.es>
2020-04-14 17:59:56 +0200

    Inicio V4

---
+++ V4/Clock.c
@@ -0,0 +1,26 @@
+#include "Clock.h"
+#include "Processor.h"
+#include "ComputerSystemBase.h"
+#include "OperatingSystemBase.h"
+int tics=0;
+
+
+void Clock_Update() {
+
+	tics++;
+    // ComputerSystem_DebugMessage(97,CLOCK,tics);
+	if(tics%intervalBetweenInterrupts==0){
+		Processor_RaiseInterrupt(CLOCKINT_BIT);
+	}
+	//Para establecer una pausa en la depuracion cuando se quiera
+	int puntoDePausa=261;
+	if(tics == puntoDePausa){
+		puntoDePausa++;
+	}
+}
+
+
+int Clock_GetTime() {
+
+	return tics;
+}
